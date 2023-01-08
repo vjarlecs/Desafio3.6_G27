7e542b669a6297762d3efb303282bff9cdeaa1db
Vjarlecs <v.jara.lecaros@gmail.com>
2023-01-07 21:02:00 -0300

    first commit

--- assets/jsc.js
+++ assets/jsc.js
@@ -3,71 +3,76 @@ const filterCurrencies = ['dolar', 'euro', 'uf', 'utm'];
 const divResult = document.querySelector('#result');
 const selectWithCurrencies = document.querySelector('#currency')
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+const grafico = document.getElementById('myChart');
+let showgraph = '';
 
 
 // obtener las monedas desde la API
-const getCurrencies = async () =>{  
+const getCurrencies = async () => {
     try {
-    const reqCurrencies = await fetch(urlApi);
-    const resData = await reqCurrencies.json();
+        const reqCurrencies = await fetch(urlApi);
+        const resData = await reqCurrencies.json();
 
-// obtener el codigo de las monedas    
-    const currencyList = filterCurrencies.map((currency) =>{
-    return {
-        code: resData[currency].codigo,
-        value: resData[currency].valor,
-    }    
-    });
+        // obtener el codigo de las monedas    
+        const currencyList = filterCurrencies.map((currency) => {
+            return {
+                code: resData[currency].codigo,
+                value: resData[currency].valor,
+            }
+        });
 
-// mostrar las monedas en el select
-currencyList.forEach((currency) =>{
-    const option = document.createElement('option');
-    option.value = currency.value;
-    option.text = capitalize(currency.code);
-    selectWithCurrencies.appendChild(option);
-});
-} catch (error) {
-console.log(error);
-alert('Error al Obtener las Monedas')
-}
+        // mostrar las monedas en el select
+        currencyList.forEach((currency) => {
+            const option = document.createElement('option');
+            option.value = currency.value;
+            option.text = capitalize(currency.code);
+            selectWithCurrencies.appendChild(option);
+        });
+    } catch (error) {
+        console.log(error);
+        alert('Error al Obtener las Monedas')
+    }
 };
 
 
 // calcular el resultado
 const calcResult = (amount, currency) => {
- divResult.innerHTML = `$ ${(amount / currency).toFixed(2)} .-`;
+    divResult.innerHTML = `$ ${(amount / currency).toFixed(2)} .-`;
 };
 
 //dibujar el grafico
 const drawChart = async (currency) => {
     try {
         const reqChart = await fetch(`${urlApi}/${currency}`);
-        const dataChart = await reqChart.json(); 
-        
+        const dataChart = await reqChart.json();
+
         const serieToChart = dataChart.serie.slice(0, 10);
         console.log(serieToChart);
-    
-       
-    // crear grafico
-     
+
+
+        // crear grafico
+
         const data = {
             labels: serieToChart.map((item) => item.fecha.substring(0, 10)),
             datasets: [{
-            label: currency,
-            data: serieToChart.map((item) => item.valor),
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1
-      }]
-    };
+                label: currency,
+                data: serieToChart.map((item) => item.valor),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        };
         const config = {
-        type: 'line',
-        data: data,
-      };
-    
-        const chartDOM = document.querySelector('#chart');
-        new Chart(chartDOM, config);
-        
+            type: 'line',
+            data: data,
+        };
+
+        showgraph += `<canvas id="chart"></canvas>`
+        grafico.innerHTML = showgraph;
+        showgraph = '';
+        new Chart(chart, config);
+
+
     } catch (error) {
         alert('Error al Obtener el Gráfico')
         console.log(error);
@@ -77,19 +82,18 @@ const drawChart = async (currency) => {
 
 document.querySelector('#btnConvert').addEventListener('click', () => {
     const amountPesos = document.querySelector('#pesos').value;
-    if(amountPesos === ''){
+    if (amountPesos === '') {
         alert('Debes ingresar un numero para Calcular');
         return;
     }
     const currencySelected = selectWithCurrencies.value;
-    const codeCurrencySelected =  
-    selectWithCurrencies.options[selectWithCurrencies.selectedIndex
-    ].text.toLowerCase();
-    
+    const codeCurrencySelected =
+        selectWithCurrencies.options[selectWithCurrencies.selectedIndex
+        ].text.toLowerCase();
+
     calcResult(amountPesos, currencySelected);
     drawChart(codeCurrencySelected);
 });
 
 
 getCurrencies();
- 
